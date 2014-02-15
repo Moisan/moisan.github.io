3fcb71e7445715abb3facd0038c5cb8327f00c76
Thierry Moisan <thierry.moisan@gmail.com>
2014-02-15 15:18:43 -0500

    update Font Awesome version

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <title>Thierry Moisan</title>
     <meta name="author" content="Thierry Moisan">
     <link rel="stylesheet" href="css/screen.css" type="text/css" />
-    <link rel="stylesheet" href="css/font-awesome.css" type="text/css" />
+    <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
   </head>
 
   <body>
@@ -21,7 +21,7 @@
 
       <div id="home">
 	<div id="aboutMe">
-	  <h1><i class="icon-user"></i> About me</h1>
+	  <h1><i class="fa fa-user"></i> About me</h1>
 	  I'm a computer science PhD student at <a href="http://www.ulaval.ca/">Université Laval</a>.
 	  I study under the supervision of <a href="http://www.ift.ulaval.ca/~quimper">Claude-Guy Quimper</a> and <a href="http://www.linkedin.com/in/jonathangaudreault"> Jonathan Gaudreault</a>.
 	  I'm part of the PPC laboratory at Université Laval.
@@ -29,24 +29,24 @@
 	  <h2>Contact</h2>
 	  <table class="contact-info" width="" cellspacing="5" cellpadding="" border="0">
 	    <tr>
-	      <td><a href="mailto:thierry.moisan.1@ulaval.ca"><i class="icon-envelope"></i> thierry.moisan.1@ulaval.ca</a></td>
-	      <td> <a href="http://twitter.com/#!/thierrymoisan"> <i class="icon-twitter-sign"></i> @thierrymoisan</a></td>
-	      <td><a href="http://ca.linkedin.com/in/thierrymoisan"><i class="icon-linkedin-sign"></i>  Linkedin</a></td>
+	      <td><a href="mailto:thierry.moisan.1@ulaval.ca"><i class="fa fa-envelope"></i> thierry.moisan.1@ulaval.ca</a></td>
+	      <td> <a href="http://twitter.com/#!/thierrymoisan"> <i class="fa fa-twitter-sign"></i> @thierrymoisan</a></td>
+	      <td><a href="http://ca.linkedin.com/in/thierrymoisan"><i class="fa fa-linkedin-sign"></i>  Linkedin</a></td>
 	    </tr>
 	    <tr>
-	      <td><a href="https://github.com/Moisan"><i class="icon-github-sign"></i>  Github</a></td>
+	      <td><a href="https://github.com/Moisan"><i class="fa fa-github-sign"></i>  Github</a></td>
 	      <td><a href="http://ulaval.academia.edu/ThierryMoisan">Academia.edu</a></td>
 	    </tr>
 	  </table>
 	</div>
 	
 	<div id="searchInterests">
-	  <h1><i class="icon-search"></i> Research interests</h1>
+	  <h1><i class="fa fa-search"></i> Research interests</h1>
 	  My reasearch interests include constraint programming and parallelism.
 	  More specifically, I work on search strategy based parallelism in problems defined by constraints.
 	</div>
 	
-	<h1><i class="icon-book"></i> Publications</h1>
+	<h1><i class="fa fa-book"></i> Publications</h1>
 	<h2>Workshop</h2>
 	<h3>2011</h3>
 	<ul>
@@ -55,7 +55,7 @@
 	  </li>
 	</ul>
 	<div id="services">
-	  <h1><i class="icon-random"></i> Services</h1>
+	  <h1><i class="fa fa-random"></i> Services</h1>
 	  <ul>
 	    <li>Publicity Chair for the <a href="http://www.cp2012.org/">CP 2012 conference</a></li>
 	    <li>Reviewer for the <a href="http://www.cp2012.org/">CP 2012 conference</a></li>
